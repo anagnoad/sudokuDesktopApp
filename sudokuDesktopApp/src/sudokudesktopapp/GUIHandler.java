85a464c44dbaf816e9706dccfe54d62e928a205a
Steve Laskaridis <stevelaskaridis@gmail.com>
2014-01-20 12:54:38 +0200

    started sudoku GUI
    gettingStartedPanel and newUserPanel

--- sudokuDesktopApp/src/sudokudesktopapp/GUIHandler.java
+++ sudokuDesktopApp/src/sudokudesktopapp/GUIHandler.java
@@ -4,11 +4,42 @@
  */
 package sudokudesktopapp;
 
+import javax.swing.JFrame;
+
 /**
  * The controller of all the GUI elements in the application.
  * Follows the MVC paradigm.
  * @author Steve
  */
 public class GUIHandler {
+    // create the application instance class
+    private Application myApp; // access modifier may need to change
+    private GettingStartedPanel gettingStartedPanel;
+    private NewUserPanel newUserPanel;
+    
+    
+    public void showAddNewUser() // is supposed to be called when the user wishes to create new user
+    {
+        // if the action is coming from the gettingStartedPanel
+        if (this.gettingStartedPanel.isVisible())
+            this.gettingStartedPanel.setVisible(false);
+        
+        // show the panel to create the new user
+        this.newUserPanel = new NewUserPanel();
+        this.myApp.add(this.newUserPanel);
+        this.newUserPanel.setVisible(true);
+    }
+    
+    public void addNewUser(String nickname)
+    {
+        // add the user to the db saved in the application instance
+    }
+    
+    public void logIn() // is supposed to be called when the user wishes to login
+    {
+    
+    }
+    
+    
     
 }
